6c950b060c803bc62645e4b084913f3cd9c8d386
AsadGulzar <Asadgujjar255@gmail.com>
2026-04-27 14:27:44 +0500

    fer sy changing yar

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ class MyHandler(BaseHTTPRequestHandler):
 
             self.send_response(200)
             self.send_header("Content-type", "text/html")
-            
             self.end_headers()
 
             response = f"<h2>Hello, {name}!</h2>"
